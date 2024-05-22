c12f9c1c5ef5158dad9d69b0756a7739e6db44f2
john layton <johnstewartlayton@gmail.com>
2024-05-22 23:16:42 +1000

    update infra

--- infra/src/main/java/org/essexstreet/Main.java
+++ infra/src/main/java/org/essexstreet/Main.java
@@ -1,11 +1,12 @@
 package org.essexstreet;
 
+import com.example.AppStack;
 import software.amazon.awscdk.App;
 
 public class Main {
     public static void main(final String[] args) {
         App app = new App();
-        new AppStack_Old(app, "MicronautAppStack");
+        new AppStack(app, "MicronautAppStack");
         app.synth();
     }
 }
